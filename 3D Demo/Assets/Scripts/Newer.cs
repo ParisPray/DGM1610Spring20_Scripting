c70226b182d6074d0042e0a9fe9ec363552d5d31
Paris Pray <paris.pray@gmail.com>
2020-01-22 11:24:00 -0700

    Updated 3D Demo

---
+++ 3D Demo/Assets/Scripts/Newer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Newer : MonoBehaviour
+{
+
+    public float floatNumber;
+    public int scoreValue;
+    public UnityEvent uEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uEvent.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(floatNumber);
+    }
+}
