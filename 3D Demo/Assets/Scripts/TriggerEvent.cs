429fa782d714eb19989d7f1716b7614a55b50d5b
Paris Pray <paris.pray@gmail.com>
2020-01-27 11:17:11 -0700

    3D Demo- learned OnTrigger

---
+++ 3D Demo/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour
+{
+
+    public UnityEvent triggerEnterEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
