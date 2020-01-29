e93a2235d5246a02320a3467da9f0bea17bd3192
Paris Pray <paris.pray@gmail.com>
2020-01-29 11:21:29 -0700

    Practicing Unity Events

---
+++ 3D Demo/Assets/Scripts/MorePractice.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MorePractice : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent, mouseOverEvent, mouseExitEvent, mouseEnterEvent;
+ 
+
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+
+    }
+
+    private void OnMouseOver()
+    {
+        mouseOverEvent.Invoke();
+    }
+
+    private void OnMouseExit()
+    {
+        mouseExitEvent.Invoke(); 
+    }
+    private void OnMouseEnter()
+    {
+        mouseEnterEvent.Invoke;
+    }
+}
