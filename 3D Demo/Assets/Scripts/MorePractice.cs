beb0b186a809548fb7bf46690bac0adf2dd75c53
Paris Pray <paris.pray@gmail.com>
2020-01-30 23:29:12 -0700

    Practiced UnityEvents and Added TextMeshPro to practice UI along with UnityEvents

--- 3D Demo/Assets/Scripts/MorePractice.cs
+++ 3D Demo/Assets/Scripts/MorePractice.cs
@@ -6,8 +6,12 @@ using UnityEngine.Events;
 public class MorePractice : MonoBehaviour
 {
     public UnityEvent mouseDownEvent, mouseOverEvent, mouseExitEvent, mouseEnterEvent;
+    private float horizontalInput;
+    private float forwardInput;
+    public float speed = 5;
+    public float turnSpeed = 5;
  
-
+    // unity events.
     private void OnMouseDown()
     {
         mouseDownEvent.Invoke();
@@ -25,6 +29,17 @@ public class MorePractice : MonoBehaviour
     }
     private void OnMouseEnter()
     {
-        mouseEnterEvent.Invoke;
+        mouseEnterEvent.Invoke();
+    }
+
+    // basic controls on player character in order to allow testing of unityevents to flow easier.
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
     }
 }
