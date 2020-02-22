fd829392f7a98db09b2be9af83b00aef0e7807df
Paris Pray <paris.pray@gmail.com>
2020-02-21 23:55:15 -0700

    Practiced with Lighting scripts and with CharacterController and SpriteController.

--- 3D Demo/Assets/Scripts/Lightscript.cs
+++ 3D Demo/Assets/Scripts/Lightscript.cs
@@ -12,7 +12,7 @@ public class Lightscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        lightObj.intensity = 0f;
+        lightObj.intensity = 1f;
     }
 
     // Update is called once per frame
--- 3D Demo/Assets/Scripts/SpriteController.cs
+++ 3D Demo/Assets/Scripts/SpriteController.cs
@@ -9,17 +9,20 @@ public class SpriteController : MonoBehaviour
 {
     public Color spriteColor = Color.red;
     private SpriteRenderer sprite;
-
+    private CharacterController controller;
+    public float Speed = 20f;
     // Start is called before the first frame update
     void Start()
     {
         sprite = GetComponent < SpriteRenderer >();
-
+        controller = GetComponent<CharacterController>();
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * Speed);
     }
 }
