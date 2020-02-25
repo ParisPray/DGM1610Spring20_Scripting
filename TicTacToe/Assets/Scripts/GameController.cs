fd55ff0023b422b130299bbaabbbf97e038494e4
Paris Pray <paris.pray@gmail.com>
2020-02-24 22:06:38 -0700

    Added Practice TicTacToe UI- coded all the way up to the EndTurn() and figuring out how to assign player and computer sides.

---
+++ TicTacToe/Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text[] buttonList;
+
+// FOR MY PROFESSOR IF HES READING THIS: hi!!! :D
+
+    // Function for looping through all the buttons
+    void SetGameControllerReferenceOnButtons()
+    {
+        for(int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
+    }
+
+    private void Awake()
+    {
+        SetGameControllerReferenceOnButtons();
+    }
+
+    // assigns a symbol to a player side
+
+    public string GetPlayerSide()
+    {
+        return "?";
+    }
+
+    // future site of the "ending your turn" function.
+    public void EndTurn()
+    {
+        Debug.Log("EndTurn is not implemented!");
+    }
+}
---
+++ TicTacToe/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public Text buttonText;
+    private GameController gameController;
+    
+        // makes it so that when you click on a button it deactivates into a visual of your move, calls the function to establish your "side", and ends your turn.
+    public void SetSpace()
+    {
+        buttonText.text = gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+
+    // Associates controller with all buttons
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+  
+}
