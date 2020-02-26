4a22a62163314bfbaa923a6039dfd27365067f5f
Paris Pray <paris.pray@gmail.com>
2020-02-25 23:17:33 -0700

    TicTacToe UI- Implemented EndTurn

--- TicTacToe/Assets/Scripts/GameController.cs
+++ TicTacToe/Assets/Scripts/GameController.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public Text[] buttonList;
+    private string playerSide;
 
 // FOR MY PROFESSOR IF HES READING THIS: hi!!! :D
 
@@ -21,18 +22,66 @@ public class GameController : MonoBehaviour
     private void Awake()
     {
         SetGameControllerReferenceOnButtons();
+        playerSide = "X";
     }
 
     // assigns a symbol to a player side
 
     public string GetPlayerSide()
     {
-        return "?";
+        return playerSide;
     }
 
-    // future site of the "ending your turn" function.
+    // Checks the top row to see if all spaces match the player.
     public void EndTurn()
     {
-        Debug.Log("EndTurn is not implemented!");
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+    }
+    void GameOver()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+
+        }
+
     }
 }
