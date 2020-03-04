2bfc8d98540b3ae0b6d567398dad9b6de587be1f
Paris Pray <paris.pray@gmail.com>
2020-03-04 11:14:46 -0700

    3D Demo- Vector3Data and Checkpoints

---
+++ 3D Demo/Assets/Scripts/VectorData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class VectorData : ScriptableObject
+{
+    public Vector3 value;
+
+    public void ChangePosition (Transform transformObj)
+    {
+        transformObj.position = value;
+    }
+
+    public void ChangeVector3Data (Vector3Data vector3DataObj)
+    {
+        vector3DataObj.value = value;
+    }
+}
+
+
+   
