840f5afb348871d79de4bea66eea11b92b2b14ad
Paris Pray <paris.pray@gmail.com>
2020-03-11 11:13:43 -0600

    3D Demo- Lists, Arrays, and Loops (Needs Improvement)

---
+++ 3D Demo/Assets/CollectableList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CollectableList : ScriptableObject
+{
+    // Start is called before the first frame update
+    public List<Collectable> collectables;
+}
