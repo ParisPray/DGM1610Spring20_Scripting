5ce8717a310d5cb875a602a4612e16de7cfc7e35
Paris Pray <paris.pray@gmail.com>
2020-03-28 22:17:02 -0600

    FinalV2- Fixed Character Controller, added temp background for testing

--- FinalTry2_2dRoguelike_Paris/Assets/Scripts/Player.cs
+++ FinalTry2_2dRoguelike_Paris/Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class CharacterController : MonoBehaviour
+public class Player : MonoBehaviour
 {
     private float horizontalInput;
     private float forwardInput;
