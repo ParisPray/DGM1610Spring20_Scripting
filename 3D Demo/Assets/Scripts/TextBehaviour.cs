d0361f84e51cddeb22a654aa66465369d430a643
Paris Pray <paris.pray@gmail.com>
2020-03-30 11:22:45 -0600

    3D Demo- Coroutines

---
+++ 3D Demo/Assets/Scripts/TextBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+    [RequireComponent(typeof(Text))]
+
+    public class TextBehaviour : MonoBehaviour
+    {
+        private Text textObj;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        textObj = GetComponent<Text>();
+        }
+
+    public void ChangeText(IntData obj)
+    {
+        textObj.text = obj.value.ToString();
+    }
+    }
+
+
