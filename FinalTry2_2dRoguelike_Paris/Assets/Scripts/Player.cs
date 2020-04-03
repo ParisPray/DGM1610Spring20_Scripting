9f4e365c8b845cf723517854a62b2884d320617f
Paris Pray <paris.pray@gmail.com>
2020-04-02 22:05:12 -0600

    FinalV2- CharacterController still not working

--- FinalTry2_2dRoguelike_Paris/Assets/Scripts/Player.cs
+++ FinalTry2_2dRoguelike_Paris/Assets/Scripts/Player.cs
@@ -3,38 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Sprite))]
+
 public class Player : MonoBehaviour
 {
-    private float horizontalInput;
-    private float forwardInput;
-    public float speed = 5f;
-    public float turnSpeed = 5f;
-    public Vector3 positionDirection;
-    public CharacterController Controller;
-    public BoxCollider playerBox;
-    public float gravity = -10f;
+    public Color spriteColor = Color.red;
+    private SpriteRenderer sprite;
+    private CharacterController controller;
+    public float Speed = 20f;
+    public Vector2 positionDirection;
     public float jumpForce = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();
+        controller = GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        positionDirection.x = Input.GetAxis("Horizontal") * speed;
-        Controller.Move(motion: positionDirection * Time.deltaTime);
-        positionDirection.y = gravity;
-        
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * Speed);
+
         if (Input.GetButtonDown("Jump"))
         {
             positionDirection.y = jumpForce;
         }
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        
+
     }
 }
+
